49f76c6ab8a8df6f2fbd1f262e633d7e2f7c4e49
Michael Yogar <michaelyogar878@gmail.com>
2023-11-09 17:16:27 -0500

    Init Spring Boot API

---
+++ src/main/java/com/michaelyogar/pokemonreviewsystem/PokemonReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.michaelyogar.pokemonreviewsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonReviewSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelyogar/pokemonreviewsystem/PokemonReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelyogar.pokemonreviewsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
